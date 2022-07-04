dbcd77b4adb8c39a53211b94a28de642fda515ba
Brandon Bode <brandonbode25@gmail.com>
2022-07-04 13:17:28 -0400

    Updated path for the public directory.

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Rendering the home.ejs file within the views directory
 app.get('/', function (req, res) {
