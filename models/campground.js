c0fc82960bf1597f231c9c4d7e6c501e027c637f
Brandon Bode <brandonbode25@gmail.com>
2022-07-04 11:23:12 -0400

    Added Mongoose middleware to delete reviews on the removal of a campground.

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const { Schema } = mongoose;
 
 //defined starting Campground schema
@@ -14,6 +15,17 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+});
+
+//Allows for the deletion of reviews on removal of Campground
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        });
+    }
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
