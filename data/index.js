2b166cc4f961aec8df7556de4b6a01aaac8be2a0
Brandon Bode <brandonbode25@gmail.com>
2022-07-04 17:55:35 -0400

    Included a hard coded author for seed data.

--- data/index.js
+++ data/index.js
@@ -21,6 +21,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 30 + 10);
         const camp = new Campground({
+            author: '62c3548eea9ad804dd7236d7',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: `https://source.unsplash.com/collection/973270`,
