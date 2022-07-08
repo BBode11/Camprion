50d8c04faac8e53b24f0c8759abd1d95d7b31a90
Brandon Bode <brandonbode25@gmail.com>
2022-07-07 21:15:16 -0400

    Add require statements for Multer/Cloudinary. Implemented Cloudinary API.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,12 +4,19 @@ const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utilities/catchAsync');
 const Campground = require('../models/campground');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 router.route('/')
     //Rendering campgrounds/index.js file within views directory
     .get(catchAsync(campgrounds.index))
     //Post request for saving newly created campgrounds
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.create));
+    //.post(isLoggedIn, validateCampground, catchAsync(campgrounds.create));
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files);
+        res.send("it worked");
+    });
 
 //Rendering create form for campgrounds
 router.get('/create', isLoggedIn, campgrounds.renderNewForm);
